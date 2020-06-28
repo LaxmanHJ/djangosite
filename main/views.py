e70de4106e51fbc13b2e06e6a46334157085c980
Lucky <51163246+LaxmanHJ@users.noreply.github.com>
2020-06-29 00:29:49 +0530

    added sqlLite3 db

--- main/views.py
+++ main/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import ToDoList, Item
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Dj an go Lucky!<h1/>")
+def index(response,id):
+    ls = ToDoList.objects.get(id=id)
+    items = ls.item_set.get(id=id)
+    return HttpResponse("<h1>%s<h1/><br></br><p>%s</p>" %(ls.name,items.text))
 
 
-def v1(response):
-    return HttpResponse("<h1>view 1!!!<h1/>")
 
 
     
